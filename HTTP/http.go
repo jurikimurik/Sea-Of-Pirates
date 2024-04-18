d771ccb8f21a5a447fb7ba8179685438d6988014
Jurikimurik <makovskyiyurii@gmail.com>
2024-04-18 22:07:30 +0200

    HTTP Basic functionality
    Network functions to implement were added.
    
    Added function Call that is responsible for sending GET, POST, DELETE HTTP requests and retrieving data from them!
    
    Functions are also commented.

--- HTTP/http.go
+++ HTTP/http.go
@@ -1,15 +1,142 @@
 package http
 
-// - TO IMPLEMENT ---------------------------------------------------------------------
-
-//func InitGame() error
-//func Board() ([]string, error)
-//func Status() (*StatusResponse, error)
-//func Fire(coord string) (string, error)
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/url"
+)
 
 // ----- GLOBAL  ----------------------------------------------------------------------
 
-var serverURL string = "https://go-pjatk-server.fly.dev"
+var token string
+var serverURL string = "https://go-pjatk-server.fly.dev/api/"
+
+const (
+	GET    = "GET"
+	POST   = "POST"
+	DELETE = "DELETE"
+)
+
+// ----- SERVER -----------------------------------------------------------------------
+
+// --- BASIC -------------------
+//func GameStatus() error
+//func StartGame() error
+//func GetMyGameBoard() error
+//func Fire(coord string) error
+
+// --- OPTIONAL ----------------
+//func GiveUp() error
+//func GetMyAndOpponentDesc() error
+//func RefreshSession() error
+//func Lobby() error
+//func Stats() error
+//func StatsOfPlayer(nick string) error
+
+// ----- NETWORK ----------------------------------------------------------------------
+
+// Call is a all-in-one function for GET, POST and DELETE HTTP Requests
+//
+//	Arguments:
+//
+// TYPE - Type of HTTP Request (GET / POST / DELETE)
+//
+// addURL - URL to add to serverURL
+//
+// parameters - Map of parameters to be inside of URL after "?"
+//
+// jsonParameters - Map of JSON data parameter of request
+//
+//	Returns:
+//
+// http.Header - Header of the HTTP request
+//
+// int - Status code after the HTTP request
+//
+// []byte - Readed body of the HTTP request
+//
+// error - Error that might occured while calling this method!
+func Call(TYPE string, addURL string, parameters map[string]string, jsonParameters map[string]any) (http.Header, []byte, int, error) {
+
+	// Creating URL with parameters
+	finalUrl := urlWithParameters(parameters, addURL)
+
+	// Creating JSON data to send
+	json_data, err := json.Marshal(jsonParameters)
+	if err != nil {
+		return nil, []byte{}, -1, err
+	}
+
+	// Preparing variables
+	var resp *http.Response
+
+	var header http.Header
+	var body []byte
+	var statusCode int
+	var errHttp error
+
+	switch TYPE {
+	case GET:
+
+		// Making the GET request
+		resp, err = http.Get(finalUrl)
+		if err != nil {
+			errHttp = err
+		}
+
+	case POST:
+
+		// Making the POST request
+		resp, err = http.Post(finalUrl, "application/json", bytes.NewBuffer(json_data))
+		if err != nil {
+			errHttp = err
+		}
+
+	case DELETE:
+		// CAUTION!! MAY BE UNSTABLE BECAUSE OF REQUESTION INSTEAD OF RESPONSE!
+		// Making the DELETE request
+		req, err := http.NewRequest(DELETE, finalUrl, bytes.NewBuffer(json_data))
+		if err != nil {
+			errHttp = err
+		}
+
+		resp = req.Response
+	}
+
+	if errHttp != nil {
+		return nil, []byte{}, -1, err
+	}
+
+	// Reading the header and body
+	body, errHttp = io.ReadAll(resp.Body)
+	header = resp.Header
+	statusCode = resp.StatusCode
+
+	resp.Body.Close()
+	return header, body, statusCode, errHttp
+}
+
+// Function for creating final URL with parameters after "?"
+//
+//	Arguments:
+//
+// parameters - parameters to add after "?".
+//
+// addURL - text to add after serverURL.
+//
+//	Returns:
+//
+// finalURL - modified URL string.
+func urlWithParameters(parameters map[string]string, addURL string) string {
+	URLParameters := url.Values{}
+	for key, value := range parameters {
+		URLParameters.Add(key, value)
+	}
+	finalUrl := serverURL + addURL + "?" + URLParameters.Encode()
+	return finalUrl
+}
 
 // ----- TESTS   ----------------------------------------------------------------------
 
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	http "sea-of-pirates/HTTP"
-	source "sea-of-pirates/Source"
+	http "sea-of-pirates/http"
+	source "sea-of-pirates/source"
+	"strconv"
 )
 
 func main() {
-	source.DrawText("Let's begin our sea battle, arr!", false)
-	source.DrawText(http.TestPackage(), true)
-	source.JSONTest()
+	//source.DrawText("Let's begin our sea battle, arr!", false)
+	//source.DrawText(http.TestPackage(), true)
+	//source.JSONTest()
+
+	//_, body, statusCode, _ := http.Call(http.GET, "stats", nil, nil)
+	//source.DrawText(string(body), true)
+	header, _, statusCode, _ := http.Call(http.POST, "game", nil, source.JSONGetDummy())
+	source.DrawText(header.Get("x-auth-token"), true)
+	source.DrawText(strconv.Itoa(statusCode), true)
 }
