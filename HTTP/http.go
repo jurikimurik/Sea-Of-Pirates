78629d8db0abfce306aa48c7509b9815dfc9c844
Jurikimurik <makovskyiyurii@gmail.com>
2024-04-25 13:10:24 +0200

    HTTP - call function making requests
    In HTTP component, call function is now making requests instead of using default functions for GET, POST and DELETE.

--- HTTP/http.go
+++ HTTP/http.go
@@ -3,6 +3,7 @@ package http
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 	"net/http"
 	"net/url"
@@ -38,10 +39,61 @@ type Response struct {
 // ----- SERVER -----------------------------------------------------------------------
 
 // --- BASIC -------------------
-//func GameStatus() error
-//func StartGame() error
-//func GetMyGameBoard() error
-//func Fire(coord string) error
+
+// GameStatus is a http function for handling game status HTTP request
+//
+//	Returns:
+//
+// Response - All in one structure that have neccessary info of HTTP Request
+func GameStatus() Response {
+	return call(GET, "game", nil, nil, true)
+}
+
+// StartGame is a http function for sending HTTP request of beginning the game.
+// It also retrives authorization token!
+//
+//	Arguments:
+//
+// jsonParameters - Map of JSON data parameter of request
+//
+//	Returns:
+//
+// Response - All in one structure that have neccessary info of HTTP Request
+func StartGame(jsonParameters map[string]any) Response {
+	resp := call(POST, "game", nil, jsonParameters, false)
+	//Check if some error occured
+	if resp.Err != nil {
+		return resp
+	}
+
+	//Retrieving authorization token
+	token = resp.Header.Get("X-Auth-Token")
+	if token == "" {
+		resp.Err = errors.New("can't retrieve authorization token from request")
+	}
+
+	//All good
+	return resp
+}
+
+// GetMyGameBoard is a http function for handling getting the game board in HTTP request
+//
+//	Returns:
+//
+// Response - All in one structure that have neccessary info of HTTP Request
+func GetMyGameBoard() Response {
+	return call(GET, "game/board", nil, nil, true)
+}
+
+// Fire is a http function for sending HTTP request of firing to specific coordinate
+//
+//	Returns:
+//
+// Response - All in one structure that have neccessary info of HTTP Request
+func Fire(coord string) Response {
+	parameters := map[string]string{"coord": coord}
+	return call(POST, "game/fire", parameters, nil, true)
+}
 
 // --- OPTIONAL ----------------
 //func GiveUp() error
@@ -65,10 +117,12 @@ type Response struct {
 //
 // jsonParameters - Map of JSON data parameter of request
 //
+// includeToken - Should token be includen into HTTP request
+//
 //	Returns:
 //
 // Response - All in one structure that have neccessary info of HTTP Request
-func Call(TYPE string, addURL string, parameters map[string]string, jsonParameters map[string]any) Response {
+func call(TYPE string, addURL string, parameters map[string]string, jsonParameters map[string]any, includeToken bool) Response {
 
 	// Creating URL with parameters
 	finalUrl := urlWithParameters(parameters, addURL)
@@ -87,11 +141,13 @@ func Call(TYPE string, addURL string, parameters map[string]string, jsonParamete
 	var statusCode int
 	var errHttp error
 
+	var req *http.Request
+
 	switch TYPE {
 	case GET:
 
 		// Making the GET request
-		resp, err = http.Get(finalUrl)
+		req, err = http.NewRequest(GET, finalUrl, bytes.NewBuffer(json_data))
 		if err != nil {
 			errHttp = err
 		}
@@ -99,22 +155,28 @@ func Call(TYPE string, addURL string, parameters map[string]string, jsonParamete
 	case POST:
 
 		// Making the POST request
-		resp, err = http.Post(finalUrl, "application/json", bytes.NewBuffer(json_data))
+		req, err = http.NewRequest(POST, finalUrl, bytes.NewBuffer(json_data))
 		if err != nil {
 			errHttp = err
 		}
 
 	case DELETE:
-		// CAUTION!! MAY BE UNSTABLE BECAUSE OF REQUESTION INSTEAD OF RESPONSE!
+
 		// Making the DELETE request
-		req, err := http.NewRequest(DELETE, finalUrl, bytes.NewBuffer(json_data))
+		req, err = http.NewRequest(DELETE, finalUrl, bytes.NewBuffer(json_data))
 		if err != nil {
 			errHttp = err
 		}
-
-		resp = req.Response
 	}
 
+	// Adding information to header
+	req.Header.Add("Content-Type", "application/json")
+	if includeToken {
+		req.Header.Add("X-AUTH-TOKEN", token)
+	}
+
+	//Making an HTTP request
+	resp, errHttp = http.DefaultClient.Do(req)
 	if errHttp != nil {
 		return Response{nil, []byte{}, -1, err}
 	}
@@ -161,6 +223,10 @@ func TestPackage() string {
 
 func GetServerURL() string {
 	return serverURL
+}
+
+func GetToken() string {
+	return token
 }
 
 // ----- SETTERS ----------------------------------------------------------------------
