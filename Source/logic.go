987708446f2b5ab27f1b518f445b763a0fa3a4b6
Jurikimurik <makovskyiyurii@gmail.com>
2024-04-25 16:10:10 +0200

    Terminal version created
    Beginner terminal version of an application was created.
    
    It supports default gameplay with previously created board and user. All game is inside of terminal without any serious GUI (text-only).

--- Source/logic.go
+++ Source/logic.go
@@ -4,7 +4,139 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	http "sea-of-pirates/HTTP"
+	"time"
 )
+
+// ----- GLOBAL  ----------------------------------------------------------------------
+
+var shots []string
+
+// ----- GAME    ----------------------------------------------------------------------
+
+// BeginGame is a function that start the whole game process.
+func BeginGame() {
+	response := http.StartGame(JSONGetDummy())
+	if response.Err != nil {
+		errorOccured(response.Err)
+	}
+
+	for {
+		status := prepareGame()
+		if jsonGetParam(status, "game_status") == "game_in_progress" {
+			break
+		}
+		jsonPrint(status)
+		WaitSecond()
+	}
+
+	enterGameFlow()
+}
+
+// prepareGame is a function that is responsible for pre-game preparations.
+//
+// It is also responsible for showing status screen and can be called many times!
+//
+// Returns:
+//
+//	map[string]any - Body of HTTP request as map
+func prepareGame() map[string]any {
+	response := http.GameStatus()
+	if response.Err != nil {
+		errorOccured(response.Err)
+		return map[string]any{}
+	}
+
+	return jsonToMap(response.Body)
+}
+
+// enterGameFlow is a function that is responsible for in-game flow.
+//
+// It waits, consumes input and is resposible for displaying the screen
+func enterGameFlow() {
+
+	//Battleship area setup
+	setupBoard := http.GetMyGameBoard()
+	if setupBoard.Err != nil {
+		errorOccured(setupBoard.Err)
+	}
+
+	jsonPrint(jsonToMap(setupBoard.Body))
+
+	//Real game flow (loop)
+	for {
+		status := http.GameStatus()
+		if status.Err != nil {
+			errorOccured(status.Err)
+			return
+		}
+
+		//Checks for game end
+		dataMap := jsonToMap(status.Body)
+		if jsonGetParam(dataMap, "game_status").(string) == "ended" {
+			break
+		}
+
+		//If there is no "should_fire" param, wait for your turn
+		if !jsonCheckParam(dataMap, "should_fire") {
+			WaitSecond()
+			continue
+		}
+
+		// Get opponents shots coordinates
+		enemyShots, assert := dataMap["opp_shots"].([]interface{})
+		if !assert {
+			errorOccured(errors.New("caution: assertion of enemyShots is not successful"))
+		}
+		// Show up opponents shots coordinates
+		DrawText("Opponents shots: ", true)
+		for _, value := range enemyShots {
+			DrawText(value.(string)+", ", false)
+		}
+		DrawText("\n", true)
+
+		DrawText("Your shots: ", true)
+		for _, value := range shots {
+			DrawText(value+", ", false)
+		}
+		DrawText("\n", true)
+		// Show text that it is your turn
+		DrawText("Your turn!", true)
+		// Take input of coordinates
+		input := ReadInput()
+		// Send Fire as HTTP request
+		response := http.Fire(input)
+		if response.Err != nil {
+			errorOccured(response.Err)
+		}
+		// If shot were accepted by server, add it to the shots
+		if response.StatusCode == 200 {
+			shots = append(shots, input)
+		}
+
+		jsonPrint(jsonToMap(response.Body))
+	}
+}
+
+// EndOfGame is responsible for ending battleship game.
+//
+// It also prints if you won or lose.
+func EndOfGame() {
+	status := http.GameStatus()
+	if status.Err != nil {
+		errorOccured(status.Err)
+	}
+	dataMap := jsonToMap(status.Body)
+
+	DrawText(dataMap["last_game_status"].(string), true)
+
+	clear(shots)
+}
+
+// WaitSecond is function that forcing thread to get some sleep for 1 second.
+func WaitSecond() {
+	time.Sleep(1 * time.Second)
+}
 
 // ----- TEXTS   ----------------------------------------------------------------------
 
@@ -66,7 +198,7 @@ func jsonToMap(j []byte) map[string]any {
 // Funciton for JSON testing
 func JSONTest() {
 	dummy := JSONGetDummy()
-	DrawText(jsonGetParam(dummy, "desk"), true)
+	DrawText(jsonGetParam(dummy, "desk").(string), true)
 	jsonAddParam(dummy, "age", "21")
 	jsonModParam(dummy, "age", "121")
 	jsonPrint(dummy)
@@ -92,13 +224,13 @@ func jsonCheckParam(jmap map[string]any, param string) bool {
 }
 
 // Function for getting value of requested param in JSON
-func jsonGetParam(jmap map[string]any, param string) string {
+func jsonGetParam(jmap map[string]any, param string) any {
 	if !jsonCheckParam(jmap, param) {
 		errorOccured(errorCreate("No such parameter (" + param + ") in JSON!"))
 		return ""
 	}
 
-	return jmap[param].(string)
+	return jmap[param]
 }
 
 // Funciton for modifing parameter in given JSON
--- main.go
+++ main.go
@@ -1,18 +1,9 @@
 package main
 
 import (
-	http "sea-of-pirates/HTTP"
 	source "sea-of-pirates/source"
 )
 
 func main() {
-	//source.DrawText("Let's begin our sea battle, arr!", false)
-	//source.DrawText(http.TestPackage(), true)
-	//source.JSONTest()
-
-	resp := http.Call(http.GET, "stats", nil, nil)
-	source.DrawText(string(resp.Body), true)
-	//header, _, statusCode, _ := http.Call(http.POST, "game", nil, source.JSONGetDummy())
-	//source.DrawText(header.Get("x-auth-token"), true)
-	//source.DrawText(strconv.Itoa(statusCode), true)
+	source.BeginGame()
 }
